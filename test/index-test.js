d7e63fa8d5e8463278a795dad74db96b00fa1363
parykhan-jameel <parexan.00443431@gmail.com>
2022-09-11 22:23:10 +0300

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -3,6 +3,7 @@ describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
+  
 })
 
 describe('whisper(string)', function() {
